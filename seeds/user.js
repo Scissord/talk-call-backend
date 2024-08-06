d24958e15d81d8badf5a65034619eeaff9f8ee0a
nurdaulet <scissxrd@gmail.com>
2024-08-06 11:29:37 +0500

    fix

--- seeds/user.js
+++ seeds/user.js
@@ -8,45 +8,45 @@ export const seed = async function(knex) {
   await knex('user').insert([
     {
       id: 1,
-      name: 'Брокер 1',
-      password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
-      role: 0
-    },
-    {
-      id: 2,
-      name: 'Руководитель продажников',
+      name: 'user1',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 1
     },
     {
-      id: 3,
-      name: 'Оператор ВЛ',
+      id: 2,
+      name: 'user2',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 2
     },
     {
-      id: 4,
-      name: 'Оператор логистики',
+      id: 3,
+      name: 'user3',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 3
     },
     {
-      id: 5,
-      name: 'Руководитель логисики',
+      id: 4,
+      name: 'user4',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 4
     },
     {
-      id: 6,
-      name: 'Руководитель логисики',
+      id: 5,
+      name: 'user5',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 5
     },
     {
-      id: 7,
-      name: 'Админ главный',
+      id: 6,
+      name: 'user6',
       password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
       role: 6
+    },
+    {
+      id: 7,
+      name: 'user7',
+      password: "$2a$10$Wpr1RN.sAJpEmxNb4pKB3.j7CU4cqObQhzyES7I1Qb/QBoBATW4I2",
+      role: 7
     },
   ]);
 };
