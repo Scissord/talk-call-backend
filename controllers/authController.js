ceb52a302175f8b1631cee229f3b6c2ef66216fb
nurdaulet <scissxrd@gmail.com>
2024-08-06 12:49:27 +0500

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import * as User from "../models/user.js";
 import * as UserToken from "../models/user_token.js";
+import * as Role from "../models/role.js";
 import generateToken from '../helpers/generateToken.js';
 
 export const getAuthRoutes = async (req, res) => {
@@ -24,6 +25,9 @@ export const login = async (req, res) => {
 
     const token = generateToken(user.id);
     await UserToken.updateWhere({ user_id: user.id }, { token });
+
+    const role = await Role.getForUser(user.id);
+    user.role = role;
 
 		res.status(200).send({ message: "Successfully login", user, token });
 	}	catch (err) {
