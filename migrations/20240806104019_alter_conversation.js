3dfe1b54d6c78d51ec0a0ede0c01d279e7578317
nurdaulet <scissxrd@gmail.com>
2024-08-06 15:45:11 +0500

    fix

---
+++ migrations/20240806104019_alter_conversation.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema
+    .alterTable('conversation', (table) => {
+      table.boolean('isFavorite').defaultTo(false);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema
+    .alterTable('conversation', (table) => {
+      table.dropColumn('isFavorite');
+    });
+};
