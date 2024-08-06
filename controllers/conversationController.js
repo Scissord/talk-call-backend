56dcf7df3004b070f75e075e61572a2183dd3682
nurdaulet <scissxrd@gmail.com>
2024-08-06 18:25:41 +0500

    fix

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -37,9 +37,7 @@ export const toggleFavorite = async (req, res) => {
     const customer_id = req.params.customer_id;
     const isFavorite = req.body.isFavorite;
 
-    console.log(isFavorite, !isFavorite)
-
-    await Conversation.updateByCustomerId(customer_id, { isFavorite: !isFavorite });
+    await Conversation.updateByCustomerId(customer_id, { isFavorite });
 
 		res.status(200).send({ message: 'ok' });
 	}	catch (err) {
