65a2449bd2d9449bdcba2e664058f3eedd1d8be2
nurdaulet <scissxrd@gmail.com>
2024-08-07 15:05:26 +0500

    fix

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import * as controller from '../controllers/messageController.js';
 import protectRoute from "../middleware/protectRoute.js";
+import multer from "multer";
+import getMulterStorage from "../helpers/getMulterStorage.js";
+
+const storage = getMulterStorage();
+const upload = multer({ storage });
 
 const router = express.Router();
 
 router.get("/:conversation_id", protectRoute, controller.get);
-router.post("/:conversation_id", protectRoute, controller.create);
+router.post("/:conversation_id", protectRoute, upload.array('files'), controller.create);
 
 export default router;
