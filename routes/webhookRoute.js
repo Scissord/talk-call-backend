c059c326b2d8dc56436a33dbaf309a004c554f35
nurdaulet <scissxrd@gmail.com>
2024-08-14 12:47:55 +0500

    whatsapp webhook

--- routes/webhookRoute.js
+++ routes/webhookRoute.js
@@ -3,7 +3,7 @@ import * as controller from '../controllers/whatsAppController.js';
 
 const router = express.Router();
 
-router.get("/", controller.initialize);
-router.post("/", controller.getIncomingMessages);
+router.get("", controller.initialize);
+router.post("", controller.getIncomingMessages);
 
 export default router;
