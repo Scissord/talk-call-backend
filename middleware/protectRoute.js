ce6bd6ff914455386de0cfca0be39ca3691e87fa
nurdaulet <scissxrd@gmail.com>
2024-08-16 16:02:27 +0500

    fix

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -5,27 +5,26 @@ import * as Role from '../models/role.js';
 
 const protectRoute = async (req, res, next) => {
   try {
-    const token = req.headers['authorization'];
-    if (!token) return res.status(401).send({ error: "Unauthorized - No Token Provided" });
+    // const token = req.headers['authorization'];
+    // if (!token) return res.status(401).send({ error: "Unauthorized - No Token Provided" });
 
-    const accessToken = token.startsWith('Bearer ') ? token.slice(7) : token;
-    const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET);
-    if(!decodedAccessToken) return res.status(400).send({ error: "Invalid Token" })
+    // const accessToken = token.startsWith('Bearer ') ? token.slice(7) : token;
+    // const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+    // if(!decodedAccessToken) return res.status(400).send({ error: "Invalid Token" })
 
-    const userToken = await UserToken.findByUserId(decodedAccessToken.userId);
-    if (!userToken || accessToken !== userToken.token) {
-      return res.status(401).send({ error: "Invalid Token" });
-    }
+    // const userToken = await UserToken.findByUserId(decodedAccessToken.userId);
+    // if (!userToken || accessToken !== userToken.token) {
+    //   return res.status(401).send({ error: "Invalid Token" });
+    // }
 
-    const user = await User.findById(decodedAccessToken.userId);
-    if (!user) return res.status(401).send({ error: "User not found" });
+    // const user = await User.findById(decodedAccessToken.userId);
+    // if (!user) return res.status(401).send({ error: "User not found" });
 
-    const role = await Role.getForUser(user.id);
-    if (!role) return res.status(401).send({ error: "User role nor found" });
+    // const role = await Role.getForUser(user.id);
+    // if (!role) return res.status(401).send({ error: "User role nor found" });
 
-    user.role = role
-
-    req.user = user;
+    // user.role = role
+    // req.user = user;
     next();
   } catch (err) {
     console.log("Error in protectRoute controller", err);
