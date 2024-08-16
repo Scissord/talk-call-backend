de4f71848f3d3a7d22ed2f0524edc95a48bdd148
nurdaulet <scissxrd@gmail.com>
2024-08-16 15:24:38 +0500

    fix

--- controllers/chatAppController.js
+++ controllers/chatAppController.js
@@ -49,9 +49,11 @@ export const register = async (req, res) => {
 };
 
 export const getIncomingMessages = async (req, res) => {
-  console.log(req.body.data.message);
-  console.log(req.body.data.fromUser);
-  console.log(req.body.data.chat);
+  console.log(req.body);
+
+  console.log(req.body.data[0].message);
+  console.log(req.body.data[0].fromUser);
+  console.log(req.body.data[0].chat);
 
   return res.sendStatus(200);
 };
