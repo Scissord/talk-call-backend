d1a58055fd8bad1b07a4884f0a3e67a7743958af
nurdaulet <scissxrd@gmail.com>
2024-08-16 15:16:22 +0500

    fix

--- controllers/chatAppController.js
+++ controllers/chatAppController.js
@@ -49,4 +49,6 @@ export const register = async (req, res) => {
 
 export const getIncomingMessages = async (req, res) => {
   console.log(req.body);
+
+  return res.sendStatus(200);
 };
--- routes/chatAppRoute.js
+++ routes/chatAppRoute.js
@@ -4,6 +4,6 @@ import * as controller from '../controllers/chatAppController.js';
 const router = express.Router();
 
 router.get("/webhook", controller.register);
-router.get("/get_incoming_messages", controller.getIncomingMessages);
+router.post("/get_incoming_messages", controller.getIncomingMessages);
 
 export default router;
