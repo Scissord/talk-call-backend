e06e290b852705d4afbc4e0861becb2aa720aafb
nurdaulet <scissxrd@gmail.com>
2024-08-16 18:51:51 +0500

    fix

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -3,25 +3,25 @@ import * as Conversation from "../models/conversation.js";
 export const get = async (req, res) => {
 	try {
     const { limit, page, type, search } = req.query;
-    const { role } = req.user;
+    // const { role } = req.user;
 
     let status = 0;
 
-    if(+role.id === 1 || +role.id === 2) {
-      status = 0;
-    };
+    // if(+role.id === 1 || +role.id === 2) {
+    //   status = 0;
+    // };
 
-    if(+role.id === 3 || +role.id === 4) {
-      status = 1;
-    };
+    // if(+role.id === 3 || +role.id === 4) {
+    //   status = 1;
+    // };
 
-    if(+role.id === 5 || +role.id === 6) {
-      status = 2;
-    };
+    // if(+role.id === 5 || +role.id === 6) {
+    //   status = 2;
+    // };
 
-    if(+role.id === 7) {
-      status = 3;
-    };
+    // if(+role.id === 7) {
+    //   status = 3;
+    // };
 
     const conversations = await Conversation.get(limit, page, search, type, status);
 
