7218a269b09be92d80cb298de979a39a1a60c5bd
nurdaulet <scissxrd@gmail.com>
2024-08-16 16:13:50 +0500

    fix

--- helpers/countPagination.js
+++ helpers/countPagination.js
@@ -1,15 +1,15 @@
 import knex from '../models/knex.js';
 
 const db = knex();
-
-export default async function(table, limit, page, search, type, status) {
+// , status
+export default async function(table, limit, page, search, type) {
   const offset = (page - 1) * limit;
 
   const total = await db(table)
     .count('* as count')
     .where((q) => {
       search && q.where('name', 'ilike', `%${search}%`);
-      status && q.where('status', status);
+      // status && q.where('status', status);
       type && q.where('isFavorite', type);
     })
     .first();
--- models/conversation.js
+++ models/conversation.js
@@ -6,17 +6,19 @@ const db = knex();
 export const get = async function (limit, page, search, type, status) {
   const { offset } = await countPagination(
     'conversation', limit, page,
-    search, type, status
+    search, type
   );
+
+  // , status
 
   const conversations = await db('conversation as co')
     .select('co.*', 'cu.*')
     .leftJoin('customer as cu', 'cu.id', 'co.customer_id')
     .where((q) => {
       search && q.where('co.name', 'ilike', `%${search}%`);
-      if(status) {
-        q.where('co.status', status);
-      }
+      // if(status) {
+      //   q.where('co.status', status);
+      // };
       type && q.where('co.isFavorite', type);
     })
     .limit(limit)
