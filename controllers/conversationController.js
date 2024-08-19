e7ad76e5e6d2887d36f5c18e9d6b1bc81649c63e
nurdaulet <scissxrd@gmail.com>
2024-08-19 12:15:38 +0500

    fix

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -7,21 +7,21 @@ export const get = async (req, res) => {
 
     let status = 0;
 
-    // if(+role.id === 1 || +role.id === 2) {
-    //   status = 0;
-    // };
+    if(+role.id === 1 || +role.id === 2) {
+      status = 0;
+    };
 
-    // if(+role.id === 3 || +role.id === 4) {
-    //   status = 1;
-    // };
+    if(+role.id === 3 || +role.id === 4) {
+      status = 1;
+    };
 
-    // if(+role.id === 5 || +role.id === 6) {
-    //   status = 2;
-    // };
+    if(+role.id === 5 || +role.id === 6) {
+      status = 2;
+    };
 
-    // if(+role.id === 7) {
-    //   status = 3;
-    // };
+    if(+role.id === 7) {
+      status = 3;
+    };
 
     const conversations = await Conversation.get(limit, page, search, type, status);
 
