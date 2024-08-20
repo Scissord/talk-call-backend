32ce61c3c56057e171216a0b921a023d58c7ff4c
nurdaulet <scissxrd@gmail.com>
2024-08-20 17:48:44 +0500

    fix

---
+++ migrations/20240820124628_alter_message_4.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema
+    .alterTable('message', (table) => {
+      table.text('text').nullable().alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema
+    .alterTable('message', (table) => {
+      table.string('text', 255).nullable();
+    });
+};
