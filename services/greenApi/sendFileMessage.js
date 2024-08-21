27e70155b8ddd2b40562e2cf288e94d4672ccb22
nurdaulet <scissxrd@gmail.com>
2024-08-21 16:25:17 +0500

    fix

--- services/greenApi/sendFileMessage.js
+++ services/greenApi/sendFileMessage.js
@@ -1,28 +1,40 @@
 import * as Message from '../../models/message.js';
+import * as Attachment from '../../models/attachment.js';
 
 export default async function sendFileMessage(customer, file, conversation_id) {
-  console.log(file);
+  const url = process.env.URL + 'uploads/' + file.filename;
 
-  // const res = await axios({
-  //   url: `${process.env.GREEN_API_URL}/waInstance${process.env.INSTANCE_ID}/sendMessage/${process.env.API_TOKEN_INSTANCE}`,
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   data: {
-  //     chatId: customer.phone,
-  //     message: message
-  //   },
-  // })
+  const res = await axios({
+    url: `${process.env.GREEN_API_URL}/waInstance${process.env.INSTANCE_ID}/sendFileByUrl/${process.env.API_TOKEN_INSTANCE}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      chatId: customer.phone,
+      urlFile: url,
+      fileName: file.originalname
+    },
+  })
 
   let obj = null;
-  // if(res.status === 200) {
-  //   obj = await Message.create({
-  //     conversation_id,
-  //     text: message,
-  //     incoming: false,
-  //   });
-  // };
+
+  if(res.status === 200) {
+    obj = await Message.create({
+      conversation_id,
+      text: "",
+      incoming: false,
+    });
+
+    const attachment = await Attachment.create({
+      message_id: obj.id,
+      link: url,
+      name: file.originalname,
+      contentType: file.mimetype
+    });
+
+    obj.attachments = [attachment]
+  };
 
   return obj;
 };
