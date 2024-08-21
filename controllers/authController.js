aed6e5ddf5251e8e544fb4bb35fecb7a382815e3
nurdaulet <scissxrd@gmail.com>
2024-08-21 13:59:58 +0500

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import * as User from "../models/user.js";
 import * as UserToken from "../models/user_token.js";
 import * as Role from "../models/role.js";
-import * as ChatAppTokens from "../services/chatapp/tokens.js";
+// import * as ChatAppTokens from "../services/chatapp/tokens.js";
 import generateToken from '../helpers/generateToken.js';
 
 export const getAuthRoutes = async (req, res) => {
@@ -22,11 +22,9 @@ export const login = async (req, res) => {
     if(!user) return res.status(400).send({ message: "Такого пользователя не существует" });
 		if(!isPasswordCorrect) return res.status(400).send({ message: "Неверный пароль" });
 
-    const {
-      accessToken, accessTokenEndTime,
-      refreshToken, refreshTokenEndTime,
-      cabinetUserId
-    } = await ChatAppTokens.makeTokens(name, password);
+    let accessToken, accessTokenEndTime,
+    refreshToken, refreshTokenEndTime,
+    cabinetUserId;
 
     await UserToken.updateWhere({ user_id: user.id }, {
       token: refreshToken,
