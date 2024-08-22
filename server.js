c478d7dd826ac9cb5d8c2f47d4a90087e05a5cc0
nurdaulet <scissxrd@gmail.com>
2024-08-22 12:45:34 +0500

    fix

--- server.js
+++ server.js
@@ -15,19 +15,11 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://' + process.env.CLIENT_IP,
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'http://localhost:5173',
+    'http://' + process.env.CLIENT_IP,
+  ],
   credentials: true,
 }));
 
--- socket/socket.js
+++ socket/socket.js
@@ -11,8 +11,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      'http://' + process.env.CLIENT_IP, // IP клиента из переменной окружения
-      'http://localhost:5173' // Добавляем localhost:5173
+      'http://' + process.env.CLIENT_IP,
+      'http://localhost:5173'
     ],
     methods: ['GET', 'POST'],
   },
