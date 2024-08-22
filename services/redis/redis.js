3157466429a5a0db6191b4939f48667e1097d099
nurdaulet <scissxrd@gmail.com>
2024-08-22 16:39:26 +0500

    redis

--- services/redis/redis.js
+++ services/redis/redis.js
@@ -1,7 +1,5 @@
 import redis from 'redis';
 
-console.log(process.env.NODE_ENV === 'production')
-
 // Создайте клиент Redis
 const redisClient = redis.createClient({
   url: process.env.NODE_ENV === 'production'
