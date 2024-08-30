dac3e9f9a696363b1fc45466a7387cc965e132db
nurdaulet <scissxrd@gmail.com>
2024-08-30 16:15:47 +0500

    init

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("", protectRoute, controller.get);
 router.post("", protectRoute, controller.create);
-router.patch("/user_id", protectRoute, controller.update);
-router.delete("/user_id", protectRoute, controller.destroy);
+router.patch("/:user_id", protectRoute, controller.update);
+router.delete("/:user_id", protectRoute, controller.destroy);
 
 export default router;
