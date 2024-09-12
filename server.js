bf65669e01ab70d016926615e7642a76205ecc24
nurdaulet <scissxrd@gmail.com>
2024-09-12 16:06:56 +0500

    fix

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Access-Control-Allow-Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
 }));
 
