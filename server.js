13bbfdf0dc1b9d92c31c0a5f87fda4aa87ae2405
nurdaulet <scissxrd@gmail.com>
2024-09-12 12:08:20 +0500

    fix

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import apiRoutes from './routes/index.js';
 
 import printName from "./helpers/printName.js";
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://tc-chat.pw',
+];
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -15,10 +20,17 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-  origin: '*',
-  methods: '*',
-  allowHeaders: '*',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
 }));
 
