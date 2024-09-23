a3cbc1a2a1d3447e54454e5b87170fb56f9ca518
nurdaulet <scissxrd@gmail.com>
2024-09-23 12:09:31 +0500

    fix

--- models/customer.js
+++ models/customer.js
@@ -51,7 +51,7 @@ export const get = async function (limit, page, search, status, manager_id) {
 
 export const getForBoard = async function (status) {
   return await db('customer as c')
-    .select('c.*', 'm.text as text', 'm.created_at as created_at')
+    .select('c.*', 'm.text as text', 'm.created_at as created_at', 'u.name as manager_name')
     .leftJoin(
       db('message as m')
         .select('m.customer_id', 'm.text', 'm.id', 'm.created_at')
