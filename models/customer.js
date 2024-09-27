2924d34653111568840354ad69e78e0eca3664bc
nurdaulet <scissxrd@gmail.com>
2024-09-27 12:19:02 +0500

    fix geo

--- models/customer.js
+++ models/customer.js
@@ -9,7 +9,7 @@ export const get = async function (limit, page, search, status, manager_id) {
       'u.name as manager_name',
       'm.text as last_message_text',
       'm.created_at as last_message_date',
-      db.raw('(SELECT COUNT(*) FROM message WHERE message.customer_id = c.id AND message.isChecked = false) as unchecked_messages_count')
+      db.raw('(SELECT COUNT(*) FROM message WHERE message.customer_id = c.id AND message.isChecked = false) as counter')
     )
     .leftJoin('user as u', 'c.manager_id', 'u.id')
     .leftJoin(
