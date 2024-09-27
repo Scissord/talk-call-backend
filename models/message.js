d8712bde8572b0f090669e80daaf0610c39cb393
nurdaulet <scissxrd@gmail.com>
2024-09-27 13:49:57 +0500

    fix geo

--- models/message.js
+++ models/message.js
@@ -44,5 +44,5 @@ export const find = async function (message_id) {
 export const clear = async function (customer_id) {
   return await db('message as m')
     .where('m.customer_id', customer_id)
-    .update('m.isChecked', true)
+    .update('m.is_checked', true)
 };
