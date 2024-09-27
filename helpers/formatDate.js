ebc0ad39d579df8f6f07a3beeebb82a2720e3777
nurdaulet <scissxrd@gmail.com>
2024-09-27 17:20:21 +0500

    fix

--- helpers/formatDate.js
+++ helpers/formatDate.js
@@ -2,16 +2,17 @@ const formatDate = (createdAt) => {
   const now = new Date();
   const createdDate = new Date(createdAt);
 
-  const isToday = now.toDateString() === createdDate.toDateString();
-  const isYesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1).toDateString() === createdDate.toDateString();
+  const isToday = now.toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' }) === createdDate.toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' });
+  const isYesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+    .toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' }) === createdDate.toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' });
 
   if (isToday) {
-    return createdDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+    return createdDate.toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Asia/Almaty' });
   } else if (isYesterday) {
-    return `Вчр. ${createdDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
+    return `Вчр. ${createdDate.toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Asia/Almaty' })}`;
   } else {
-    return createdDate.toLocaleDateString('ru-RU', { day: 'numeric', month: 'long' });
-  };
+    return createdDate.toLocaleDateString('ru-RU', { day: 'numeric', month: 'long', timeZone: 'Asia/Almaty' });
+  }
 };
 
 export default formatDate;
