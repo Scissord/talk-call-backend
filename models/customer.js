eb7666d693216db5848e42af158fe4f4f23be6d9
nurdaulet <scissxrd@gmail.com>
2024-09-27 15:01:27 +0500

    fix geo

--- models/customer.js
+++ models/customer.js
@@ -35,7 +35,7 @@ export const get = async function (limit, page, search, status, manager_id) {
         q.where('c.manager_id', manager_id);
       }
     })
-    .orderBy('m.last_message_date', 'desc')
+    .orderBy('m.created_at', 'desc')
     .paginate({
       perPage: limit,
       currentPage: page,
