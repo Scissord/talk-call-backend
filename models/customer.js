31322270e2198fb5f75b21aa0a4d32067a821f82
nurdaulet <scissxrd@gmail.com>
2024-09-30 11:30:58 +0500

    fix

--- models/customer.js
+++ models/customer.js
@@ -9,11 +9,10 @@ export const get = async function (limit, page, search, status, manager_id) {
       db.raw('MAX(m.created_at) as last_message_date'),
       db.raw('(SELECT text FROM message WHERE customer_id = m.customer_id ORDER BY created_at DESC LIMIT 1) as last_message_text'),
       'c.*',
-      'u.name as manager_name',
+      db.raw('(SELECT u.name FROM user as u WHERE u.id = m.user_id ORDER BY m.created_at DESC LIMIT 1) as manager_name'),
       db.raw('(SELECT COUNT(*) FROM message WHERE message.customer_id = c.id AND message.is_checked = false) as counter')
     )
     .leftJoin('customer as c', 'm.customer_id', 'c.id')
-    .leftJoin('user as u', 'm.user_id', 'u.id')
     .where((q) => {
       if (search) {
         q.where('c.order_id', 'ilike', `%${search}%`)
@@ -25,7 +24,7 @@ export const get = async function (limit, page, search, status, manager_id) {
         q.where('c.manager_id', manager_id);
       }
     })
-    .groupBy('m.customer_id', 'c.id', 'u.name')
+    .groupBy('m.customer_id', 'c.id')
     .orderBy('last_message_date', 'desc')
     .paginate({
       perPage: limit,
@@ -35,6 +34,7 @@ export const get = async function (limit, page, search, status, manager_id) {
 
   return result.data;
 };
+
 
 export const getForBoard = async function (status) {
   return await db('message as m')
