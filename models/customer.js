3d5acbb395f494f94f2a6c761a7ef22f49c0c0d5
nurdaulet <scissxrd@gmail.com>
2024-09-30 11:46:40 +0500

    fix

--- models/customer.js
+++ models/customer.js
@@ -13,7 +13,7 @@ export const get = async function (limit, page, search, status, manager_id) {
       db.raw('(SELECT COUNT(*) FROM message WHERE message.customer_id = c.id AND message.is_checked = false) as counter')
     )
     .leftJoin('customer as c', 'm.customer_id', 'c.id')
-    .leftJoin('user as u', 'm.user_id', 'u.id')
+    .leftJoin('user as u', 'c.manager_id', 'u.id')
     .where((q) => {
       if (search) {
         q.where('c.order_id', 'ilike', `%${search}%`)
