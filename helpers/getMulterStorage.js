d687d01d0d197ba7cac6ce41535d6be6337d2bdc
nurdaulet <scissxrd@gmail.com>
2024-11-18 17:44:55 +0500

    fix

--- helpers/getMulterStorage.js
+++ helpers/getMulterStorage.js
@@ -1,8 +1,8 @@
 import multer from "multer";
 import path from "path";
 import fs from 'fs';
+import { slugify as transliteration } from "transliteration";
 
-// Функция для создания конфигурации хранилища для multer
 export default function getMulterStorage() {
   return multer.diskStorage({
     destination: (req, file, cb) => {
@@ -15,10 +15,16 @@ export default function getMulterStorage() {
       cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
-      const currentTime = new Date().toISOString().replace(/[-:.]/g, '');
+      const currentTime = new Date().toISOString().replace(/[-:.]/g, "");
       const originalName = path.basename(file.originalname, path.extname(file.originalname));
-      const uniqueSuffix = `${originalName}-${currentTime}${path.extname(file.originalname)}`;
+
+      // Транслитерация имени файла
+      const sanitizedOriginalName = transliteration(originalName);
+      
+      console.log(sanitizedOriginalName);
+      // Создание уникального имени
+      const uniqueSuffix = `${sanitizedOriginalName}-${currentTime}${path.extname(file.originalname)}`;
       cb(null, uniqueSuffix);
-    }
+    },
   });
 }
